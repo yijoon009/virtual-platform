ce838b2d1706678481df0040009b6ffec9297bdf
yijun <yijoon009@gmail.com>
2023-05-20 21:09:42 +0900

    init settings

---
+++ src/test/java/com/virtual/meetup/MeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtual.meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
