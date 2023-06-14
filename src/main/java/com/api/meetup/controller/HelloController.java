facc041f40054674443dd77642733431698a3fda
jinan <wlsdks12@naver.com>
2023-06-14 21:13:03 +0900

    1. 로그추가

--- src/main/java/com/api/meetup/controller/HelloController.java
+++ src/main/java/com/api/meetup/controller/HelloController.java
@@ -3,9 +3,11 @@ package com.api.meetup.controller;
 import com.api.meetup.entity.HelloEntity;
 import com.api.meetup.repository.HelloRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
@@ -15,6 +17,7 @@ public class HelloController {
     @GetMapping("hello")
     public String hello() {
         HelloEntity entity = helloRepository.findAll().get(0);
+        log.info("entity={}", entity);
         return "hello. world";
     }
 
